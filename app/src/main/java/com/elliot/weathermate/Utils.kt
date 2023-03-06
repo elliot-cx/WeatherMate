eec18ff9e9fe962a679f3dc9c352b1f561e335b1
CASNA ELLIOT p2103912 <derfire22@gmail.com>
2023-03-06 09:50:21 +0100

    update deps

---
+++ app/src/main/java/com/elliot/weathermate/Utils.kt
@@ -0,0 +1,25 @@
+package com.elliot.weathermate
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+
+class Utils {
+    companion object {
+
+        // Fonction pour sauvegarder les données
+        fun saveConfig(data: Boolean, context: Context) {
+            val sharedPreferences = context.getSharedPreferences("setup", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("setup", data)
+            editor.apply()
+        }
+
+        // Fonction pour charger les données
+        fun isConfigured(context: Context): Boolean {
+            val sharedPreferences = context.getSharedPreferences("setup", Context.MODE_PRIVATE)
+            return sharedPreferences.getBoolean("setup", false)
+        }
+    }
+}
