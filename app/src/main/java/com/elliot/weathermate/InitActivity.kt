314fff4c970ffc2e69366139203e1f869ba263d4
CASNA ELLIOT p2103912 <derfire22@gmail.com>
2023-03-06 09:11:54 +0100

    Add Activites

---
+++ app/src/main/java/com/elliot/weathermate/InitActivity.kt
@@ -0,0 +1,11 @@
+package com.elliot.weathermate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/elliot/weathermate/SetupActivity.kt
@@ -0,0 +1,11 @@
+package com.elliot.weathermate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}
