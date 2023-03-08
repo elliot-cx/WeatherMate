73402cffe7201580f849fca37f3ba945df093491
anne passelegue <anne.passelegue@etu.univ-lyon1.fr>
2023-03-08 11:50:18 +0100

    Avancée de la page setup (fragments)

---
+++ app/src/main/java/com/elliot/weathermate/views/setup/frags/Fragment2.kt
@@ -0,0 +1,32 @@
+package com.elliot.weathermate.views.setup.frags
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.elliot.weathermate.R
+import com.tayfuncesur.stepper.Stepper
+import kotlinx.android.synthetic.main.fragment2.*
+
+class Fragment2 : Fragment(){
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button2.setOnClickListener {
+            view.findNavController().navigate(R.id.fragment2to3)
+            activity?.findViewById<Stepper>(R.id.stepper)?.forward()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/elliot/weathermate/views/setup/frags/Fragment3.kt
@@ -0,0 +1,27 @@
+package com.elliot.weathermate.views.setup.frags
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.elliot.weathermate.R
+import com.tayfuncesur.stepper.Stepper
+import kotlinx.android.synthetic.main.fragment1.*
+
+class Fragment3 : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment3, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
