2d56f2204951915dec38a6d0ed7895b1ea21d98d
p2103912 <elliot.casna@etu.univ-lyon1.fr>
2023-03-16 20:43:27 +0100

    [UPDATE] Detail Activity

--- app/src/main/java/com/elliot/weathermate/views/detail/DetailActivity.kt
+++ app/src/main/java/com/elliot/weathermate/views/detail/DetailActivity.kt
@@ -1,36 +1,53 @@
 package com.elliot.weathermate.views.detail
 
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.elliot.weathermate.R
 import com.elliot.weathermate.Utils
+import com.elliot.weathermate.data.Weather
 import com.elliot.weathermate.data.WeatherData
-import com.elliot.weathermate.views.main.MainActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlin.math.roundToInt
 
 class DetailActivity : AppCompatActivity() {
+
+    var favorite: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val weatherDataInput = intent.getParcelableExtra<WeatherData>("weather")
+        val weatherData = intent.getParcelableExtra<WeatherData>("weather")
 
-        if(Utils.weathers.contains(weatherDataInput)){
-            switch1.isChecked = true
+        city_name_text.text = weatherData!!.name
+
+        temp_text.text =  "${weatherData.weatherInfo.temp.roundToInt()}°c"
+
+        val weather: Weather = weatherData!!.weather[0]
+        weather_text.text = "${weather.description}"
+
+        animationView.setAnimation(
+            resources.getIdentifier(
+                "${weather.main.lowercase()}${weather.id}", "raw",
+                packageName
+            )
+        )
+        animationView.playAnimation()
+
+        windSpeed.text = "${weatherData.wind.speed} m/s"
+        windOrientation.rotation = weatherData.wind.deg + 180
+
+        humidity_text.text = "${weatherData.weatherInfo.humidity}%"
+
+        // Mise en place du background en fonction du temps
+        val backgroundDrawable =  this.gradient_background.background as GradientDrawable
+        backgroundDrawable.run {
+            mutate()
+            colors = Utils.choseWeatherBackground(weather.main.lowercase())
         }
 
-        if(weatherDataInput!!.isGPS){
-            switch1.isEnabled = false
-        }
-
-        switch1.setOnCheckedChangeListener{_,isChecked ->
-            if (isChecked){
-                Utils.weathers.add(weatherDataInput)
-            }else{
-                Utils.weathers.remove(weatherDataInput)
-            }
-            Utils.saveLocations(this)
+        if(Utils.weathers.contains(weatherData)){
+            favorite = true
         }
     }
 }
